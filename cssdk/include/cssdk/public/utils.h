6df0a5ab6253ee9f12db98fff33196a0f13a6332
the_hunter <mail4thehunter@gmail.com>
2020-05-25 09:34:42 +0300

    Add cssdk_hud_message function

--- cssdk/include/cssdk/public/utils.h
+++ cssdk/include/cssdk/public/utils.h
@@ -2,7 +2,7 @@
 // Created          : 04-01-2020
 //
 // Last Modified By : the_hunter
-// Last Modified On : 04-01-2020
+// Last Modified On : 05-25-2020
 // ***********************************************************************
 //     Copyright (c) 1996-2002, Valve LLC. All rights reserved.
 // ***********************************************************************
@@ -11,6 +11,8 @@
 
 #include <cssdk/common/consts.h>
 #include <cssdk/engine/edict.h>
+
+class EntityBase;
 
 /// <summary>
 /// </summary>
@@ -26,3 +28,23 @@ T cssdk_entity_private_data(const Edict* const entity)
 {
 	return entity == nullptr ? T() : static_cast<T>(entity->private_data);
 }
+
+/// <summary>
+/// </summary>
+short cssdk_fixed_signed16(float value, float scale);
+
+/// <summary>
+/// </summary>
+unsigned short cssdk_fixed_unsigned16(float value, float scale);
+
+/// <summary>
+/// </summary>
+void cssdk_hud_message(EntityBase* entity, const HudTextParams& hud_params, const char* message, Edict* client = nullptr);
+
+/// <summary>
+/// </summary>
+void cssdk_hud_message(Edict* client, const HudTextParams& hud_params, const char* message);
+
+/// <summary>
+/// </summary>
+void cssdk_hud_message(int client, const HudTextParams& hud_params, const char* message);
