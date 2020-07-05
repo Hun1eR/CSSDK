303447ed8d1431e6dce96c63a71a3c2ca639f46e
the_hunter <mail4thehunter@gmail.com>
2020-07-05 08:36:17 +0300

    Add method Vector::distance_2d

--- cssdk/include/cssdk/public/vector.h
+++ cssdk/include/cssdk/public/vector.h
@@ -555,6 +555,15 @@ public:
 	}
 
 	/// <summary>
+	/// <para>Computes the distance between two 2D vectors (points).<br/></para>
+	/// </summary>
+	/// <returns>The distance between two vectors.<br/></returns>
+	[[nodiscard]] vec_t distance_2d(const Vector& other) const
+	{
+		return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+	}
+
+	/// <summary>
 	/// </summary>
 	void copy_to_array(vec_t vec_3d[3]) const
 	{
