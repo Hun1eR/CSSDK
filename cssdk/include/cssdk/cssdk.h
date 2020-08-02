cf80833a3a05dc5fda1af36bf8255fcd43970a48
the_hunter <mail4thehunter@gmail.com>
2020-08-02 08:20:21 +0300

    Add cssdk.h

---
+++ cssdk/include/cssdk/cssdk.h
@@ -0,0 +1,19 @@
+// ***********************************************************************
+// Created          : 08-02-2020
+//
+// Last Modified By : the_hunter
+// Last Modified On : 08-02-2020
+// ***********************************************************************
+//     Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+// ***********************************************************************
+
+#pragma once
+
+#include <cssdk/common/consts.h>
+#include <cssdk/dll/func_break.h>
+#include <cssdk/dll/game_rules.h>
+#include <cssdk/dll/gib.h>
+#include <cssdk/dll/player.h>
+#include <cssdk/engine/eiface.h>
+#include <cssdk/public/interface.h>
+#include <cssdk/public/utils.h>
